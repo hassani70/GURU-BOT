a8a5fb912ec1dbc35ba611c758101b92defb1170
hassani70 <158146792+hassani70@users.noreply.github.com>
2024-01-30 04:26:13 -0800

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['254779385582', 'WHiz', true],
+  ['254779385582', 'WHiz', true], 
   [''] 
 ] //Number of owners
 
